bdf06b54bc0c3c6d916ca0c50186bf99bdf71972
Malika01 <malika.chikara@gmail.com>
2021-11-22 19:37:02 +0530

    issues#1 solved except expr changes, added axios, ippts post req working

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -3,21 +3,82 @@ import { ThemeProvider } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'
 import React from 'react';
-import * as types from '../types/inittypes'
+import { ComputationCost, ComputationCostMatrix, ComputationCostMatrixElem, TaskGraphAdjMatrix, TaskGraphAdjMatrixElem, CommCost } from "../types/inittypes";
+import { Task } from '../types/basetypes';
 import theme from './theme'
 import Table from './table'
+import axios from 'axios'
 
 export const Home: React.FC = () => {
 
     const [nodes, setNodes] = React.useState(2)
     const [procs, setProcs] = React.useState(2)
     const [dis, setDis] = React.useState(false)
-    
-    /*React.useEffect(() => {
-        if (procs > 3) {
-            disabled = true;        
+
+    const sendData = (event) => {
+        let adjArr: TaskGraphAdjMatrix = []
+        let compArr: ComputationCostMatrix = []
+        event.preventDefault()
+        const data = new FormData(event.target)
+
+        const task = (i: number) => {
+            let task: Task = {
+                expression: data.get(`expr${i}`) as string,
+                taskId: i,
+            }
+            return (task)
         }
-    }, []);*/
+
+        for (let i = 0; i < nodes; i++){
+            let matrixelem: ComputationCostMatrixElem = {
+                task: task(i),
+                compCosts: []
+            }
+
+            let adjelem: TaskGraphAdjMatrixElem = {
+                task: task(i),
+                commCosts: []
+            }
+
+            for (let j = 0; j < procs; j++){
+                let compcost: ComputationCost = {
+                    server: {
+                        url: "",
+                        serverId: j
+                    },
+                    cost : Number(data.get(`adjcell${i}${j}`))
+                }
+                matrixelem.compCosts.push(compcost)         
+            }
+
+            for (let k = 0; k < nodes; k++){
+                let commcost: CommCost = {
+                    task: task(k),
+                    weight: Number(data.get(`adjcell${i}${k}`))
+                }
+                adjelem.commCosts.push(commcost)
+            }
+            compArr.push(matrixelem)
+            adjArr.push(adjelem)
+        }
+
+        console.log("adj matrix =", adjArr)
+        console.log("comp matrix =", compArr)
+        alert("check the console!")
+      
+        axios({
+            method: 'post',
+            url: 'http://localhost:5030/ippts',
+            data: {
+                "taskGraph": adjArr,
+                "computationCostMatrix": compArr
+            }
+        }).then(function (response) {
+            console.log("output =", response.data)
+        }).catch(function (error) {
+            console.log(error)
+        })
+    }
 
     return (
         <ThemeProvider theme={theme}>
@@ -26,15 +87,18 @@ export const Home: React.FC = () => {
                     IPPTS ALGORITHM SIMULATION
                 </h1>
                 <Container maxWidth="xl">
+
+                    <form onSubmit={sendData}>
+
                     <div style={{ marginBlock: "2em" }}>
                         <p>
                             Enter the number of Nodes(Tasks) in Graph: 
                         </p>
-                        <TextField inputProps={{ type: 'text', pattern: '[0-9]*', maxlength: '2', style: { padding: '0.5em' } }} color="primary" focused value={nodes} onChange={(e) => setNodes(Number(e.currentTarget.value))} />
+                        <TextField inputProps={{ type: 'text', pattern: '[0-9]*', maxLength: '2', style: { padding: '0.5em' } }} color="primary" focused value={nodes} onChange={(e) => setNodes(Number(e.currentTarget.value))} />
                         <p>
                             Enter the number of Servers: 
                         </p>
-                        <TextField inputProps={{ type: 'text', maxlength: '2', pattern: '[0-9]*', style: { padding: '0.5em' } }} color="primary" focused value={procs}
+                        <TextField inputProps={{ type: 'text', maxLength: '2', pattern: '[0-9]*', style: { padding: '0.5em' } }} color="primary" focused value={procs}
                             onChange={(e) => {
                                 setProcs(Number(e.currentTarget.value));
                                 if (Number(e.currentTarget.value) <= 3 && Number(e.currentTarget.value) > 0) { setDis(false) }
@@ -63,8 +127,11 @@ export const Home: React.FC = () => {
                     </table>
                     </div> 
                     </div>
-                    <Button name="run" sx={{marginInline: '2em', marginBlock: '1em'}} variant="outlined" color="warning">RUN ALGO</Button>
-                    <Button name="simulate" sx={{marginInline: '2em', marginBlock: '1em'}} disabled={dis} variant="outlined" color="warning" >SIMULATE</Button>
+
+                    <Button type="submit" name="run" sx={{ marginInline: '2em', marginBlock: '1em' }} variant="outlined" color="warning">RUN ALGO</Button>
+                    <Button type="submit" name="simulate" sx={{marginInline: '2em', marginBlock: '1em'}} disabled={dis} variant="outlined" color="warning">SIMULATE</Button>
+
+                    </form>
                    
                 </Container>
             </div>
--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,12 +1,18 @@
-const Cell = ({ row, col }: { row: number, col: number }) => (
+const CellAdj = ({ row, col }: { row: number, col: number }) => (
     <td>
-        <input id={`cell${row}${col}`} type='text' maxLength={Number('3')} />
+        <input required id={`adjcell${row}${col}`} name={`adjcell${row}${col}`} type='text' /*maxLength={Number('8')}*/ pattern='[+-]?[0-9]*' title='max 3 digit number'/>
+    </td>
+)
+
+const CellComp = ({ row, col }: { row: number, col: number }) => (
+    <td>
+        <input required id={`compcell${row}${col}`} name={`compcell${row}${col}`} type='text' /*maxLength={Number('8')}*/ pattern='[0-9]*' title='max 3 digit number'/>
     </td>
 )
 
 const Expr = ({ row }: { row: number }) => (
     <td>
-        <input id={`expr${row}`} type='text' maxLength={Number('20')} />
+        <input id={`expr${row}`} name={`expr${row}`} type='text' maxLength={Number('20')} />
     </td>
 ) 
 
@@ -16,31 +22,34 @@ function Table({ num1, num2, expr }: { num1: number, num2: number, expr: boolean
     let rows: JSX.Element[] = [];
 
     if (expr == true) {
-        cols = [];
         for (let k = 1; k < num2; k++) [
             cols.push(<td>Proc{k}</td>)
         ]
         rows.push(<tr><td></td>{cols}<td>EXPR</td></tr>)        
     }
     else {
-        cols = [];
         for (let k = 1; k <= num2; k++) [
             cols.push(<td>Task{k}</td>)
         ]
         rows.push(<tr><td></td>{cols}</tr>)
     }
 
-    for (let i = 1; i <= num1; i++){
+    for (let i = 0; i < num1; i++){
         cols = [];
-        for (let j = 1; j <= num2; j++){
-            if (expr == true && j == num2) {
-                cols.push(<Expr row={i} />)                    
+        for (let j = 0; j < num2; j++){
+            if (expr == true) {
+                if (j == num2 - 1) {
+                    cols.push(<Expr row={i} />)                    
+                }
+                else {
+                    cols.push(<CellComp row={i} col={j} />)                    
+                }
             }
             else {
-                cols.push(<Cell row={i} col={j} />)                
+                cols.push(<CellAdj row={i} col={j} />)
             }
         }
-        rows.push(<tr><td>Task{i}</td>{cols}</tr>)
+        rows.push(<tr><td>Task{i+1}</td>{cols}</tr>)
     }
 
     return (
